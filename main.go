3f30d07a652e870863c74e6d709fe13f2afb8f72
yutaroyamanaka <falcon4842@gmail.com>
2023-02-04 21:30:37 +0900

    create app func

--- main.go
+++ main.go
@@ -6,35 +6,55 @@ import (
 	"os"
 
 	log "github.com/go-kit/log"
+	"github.com/go-kit/log/level"
 
 	"github.com/spf13/cobra"
 )
 
-func main() {
-	var logger log.Logger
-	logger = log.NewLogfmtLogger(log.NewSyncWriter(os.Stdout))
+var (
+	port int
+)
+
+func NewLogger() log.Logger {
+	logger := log.NewLogfmtLogger(log.NewSyncWriter(os.Stdout))
 	logger = log.With(logger, "ts", log.DefaultTimestampUTC, "caller", log.DefaultCaller)
+	return logger
+}
 
-	var (
-		port int
-	)
-
+func app(logger log.Logger) *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "serve",
-		Short: "Run very simple HTTP server",
+		Use:   "Book API",
+		Short: "Run a Book API server",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			s := &http.Server{
+			srv := &http.Server{
 				Addr: fmt.Sprintf(":%d", port),
 				Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-					w.Write([]byte("hello world"))
+					w.Write([]byte("Hello world\n"))
 				}),
 			}
-			logger.Log("msg", "http server starts runnning", "port", port)
-			return s.ListenAndServe()
+
+			srvCh := make(chan error, 1)
+			go func() {
+				level.Info(logger).Log("msg", "http server starts runnning", "port", port)
+				if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+					level.Error(logger).Log("msg", "failed to start the server", "error", err)
+					srvCh <- err
+				}
+				close(srvCh)
+			}()
+
+			return <-srvCh
 		},
 	}
 	cmd.Flags().IntVarP(&port, "port", "p", 80, "port number that http server runs on")
+	return cmd
+}
+
+func main() {
+	logger := NewLogger()
+	cmd := app(logger)
 	if err := cmd.Execute(); err != nil {
-		logger.Log("error", err)
+		level.Error(logger).Log("msg", "failed to execute a command", "error", err)
+		os.Exit(1)
 	}
 }
