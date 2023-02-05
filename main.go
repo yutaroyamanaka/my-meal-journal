477acdd1cfe0b0026b7bd58d74851d6bdf00ade3
Yutaro Yamanaka <34850155+yutaroyamanaka@users.noreply.github.com>
2023-02-05 11:44:17 +0900

    refactoring main.go and add main_test.go (#2)

--- main.go
+++ main.go
@@ -1,40 +1,85 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"net"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 
 	log "github.com/go-kit/log"
+	"github.com/go-kit/log/level"
 
 	"github.com/spf13/cobra"
 )
 
-func main() {
-	var logger log.Logger
-	logger = log.NewLogfmtLogger(log.NewSyncWriter(os.Stdout))
+var (
+	port int
+)
+
+func NewLogger() log.Logger {
+	logger := log.NewLogfmtLogger(log.NewSyncWriter(os.Stdout))
 	logger = log.With(logger, "ts", log.DefaultTimestampUTC, "caller", log.DefaultCaller)
+	return logger
+}
 
-	var (
-		port int
-	)
+func run(ctx context.Context, l net.Listener, logger log.Logger) error {
+	srv := &http.Server{
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Write([]byte("Hello world\n"))
+		}),
+	}
 
-	cmd := &cobra.Command{
-		Use:   "serve",
-		Short: "Run very simple HTTP server",
-		RunE: func(cmd *cobra.Command, args []string) error {
-			s := &http.Server{
-				Addr: fmt.Sprintf(":%d", port),
-				Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-					w.Write([]byte("hello world"))
-				}),
+	srvCh := make(chan error, 1)
+	go func() {
+		level.Info(logger).Log("msg", "http server starts runnning", "address", l.Addr().String())
+		if err := srv.Serve(l); err != nil && err != http.ErrServerClosed {
+			level.Error(logger).Log("msg", "failed to start the server", "error", err)
+			srvCh <- err
+		}
+		close(srvCh)
+	}()
+
+	sigctx, stop := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
+	defer stop()
+	for {
+		select {
+		case err := <-srvCh:
+			return err
+		case <-sigctx.Done():
+			if err := srv.Shutdown(ctx); err != nil {
+				level.Error(logger).Log("msg", "failed to shut down the server", "error", err)
+				return err
 			}
-			logger.Log("msg", "http server starts runnning", "port", port)
-			return s.ListenAndServe()
+			return nil
+		}
+	}
+}
+
+func app(ctx context.Context, logger log.Logger) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "app",
+		Short: "Run a Book API server",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			l, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+			if err != nil {
+				level.Error(logger).Log("msg", fmt.Sprintf("failed to listen on address: %d", port), "error", err)
+				return err
+			}
+			return run(ctx, l, logger)
 		},
 	}
 	cmd.Flags().IntVarP(&port, "port", "p", 80, "port number that http server runs on")
+	return cmd
+}
+
+func main() {
+	logger := NewLogger()
+	cmd := app(context.Background(), logger)
 	if err := cmd.Execute(); err != nil {
-		logger.Log("error", err)
+		level.Error(logger).Log("msg", "failed to execute a command", "error", err)
+		os.Exit(1)
 	}
 }
---
+++ main_test.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"net"
+	"net/http"
+	"os"
+	"testing"
+
+	log "github.com/go-kit/log"
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestRun(t *testing.T) {
+	logger := log.NewLogfmtLogger(log.NewSyncWriter(os.Stderr))
+	l, err := net.Listen("tcp", ":0")
+	if err != nil {
+		t.Fatalf("failed to listen on local address: %#v", err)
+	}
+	ctx, cancel := context.WithCancel(context.Background())
+	ch := make(chan error, 1)
+	go func() {
+		ch <- run(ctx, l, logger)
+	}()
+	url := fmt.Sprintf("http://%s", l.Addr().String())
+	resp, err := http.Get(url)
+	if err != nil {
+		t.Fatalf("Got an unexpected error: %#v", err)
+	}
+	defer func() {
+		io.Copy(io.Discard, resp.Body)
+		resp.Body.Close()
+	}()
+	got, err := io.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatalf("Got an unexpected error: %#v", err)
+	}
+	want := "Hello world\n"
+	if diff := cmp.Diff(string(got), want); diff != "" {
+		t.Errorf("Got an unexcpeted response: %s", diff)
+	}
+	cancel()
+	if err := <-ch; err != nil {
+		t.Errorf("run func returned an error: %#v", err)
+	}
+}
