81205f6371286b9c3df9da869a6508c1b84cad94
yutaroyamanaka <falcon4842@gmail.com>
2023-02-25 18:41:01 +0900

    use terraform cloud instead of s3

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,28 +14,7 @@ terraform {
       name = "ci"
     }
   }
-
-  backend "s3" {
-    bucket = "bucket-yutaroyamanaka"
-    key    = "terraform"
-    region = "ap-northeast-1"
-    # this is necessary for production env
-    # dynamodb_table = "terraform-state-lock"
-  }
 }
-
-# this is necessary for production env
-/*
-resource "aws_dynamodb_table" "terraform_state_lock" {
-  name           = "terraform-state-lock"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-} */
 
 provider "aws" {
   region = "ap-northeast-1"
