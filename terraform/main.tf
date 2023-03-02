99760b5e90fd2c5bf1ce196aa1122d5ec161fad1
yutaroyamanaka <falcon4842@gmail.com>
2023-03-02 12:14:55 +0900

    add explanation of s3 and dynamodb

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,6 +7,7 @@ terraform {
   }
   required_version = ">= 1.3.9"
 
+  // prepare s3 bucket and dynamo-db in advacne.
   backend "s3" {
     bucket         = "bucket-yutaroyamanaka"
     key            = "terraform"
@@ -18,3 +19,20 @@ terraform {
 provider "aws" {
   region = "ap-northeast-1"
 }
+
+/* DO NOT APPLY FOLLOWING RESOURCES EVERY TIME
+resource "aws_s3_bucket" "terraform-backend" {
+  bucket = "bucket-yutaroyamanaka"
+  acl    = "private"
+}
+
+resource "aws_dynamodb_table" "eks-lock-table" {
+  name           = "terraform-state-lock"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+*/
